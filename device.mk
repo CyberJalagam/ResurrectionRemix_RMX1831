682895006b213353d4771d34564101bf082146c6
Danny Baumann <dannybaumann@web.de>
2018-07-18 17:25:32 +0200

    RMX1831: Enable call recording
    Change-Id: I1b4478b330b13cf5d0fe1ed0d40f43e4b3825f33

--- device.mk
+++ device.mk
@@ -2,11 +2,12 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
-	
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
