3f3036fb3c73e470ca2b42edad3293cd155432c5
dianlujitao <dianlujitao@lineageos.org>
2018-09-08 18:19:12 +0800

    RMX1831: Build vendor android.hidl.base@1.0.so
    Change-Id: I39c71e61252228dbfcf50047795ea23b2275e4bd
    Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_vendor \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
