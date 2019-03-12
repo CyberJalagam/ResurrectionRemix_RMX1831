2be31824304b51dcd5c4eaa1b514bae198d7c7a1
Brad Ebinger <breadley@google.com>
2019-03-11 17:38:38 -0700

    RMX1831: Enable FEATURE_TELEPHONY_IMS for this device.
    Bug: 118823723
    Test: checkbuild
    Merged-In: I69fd98160e852b371331a7bc169c7c97dd37f011
    Change-Id: I69fd98160e852b371331a7bc169c7c97dd37f011

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:system/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
