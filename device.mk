ee712fefcc37b11207f56bc66207f44a9db9290f
SparXFusion <s2234nadar@gmail.com>
2020-05-31 17:29:28 +0530

    RMX1831: initial import

---
+++ device.mk
@@ -0,0 +1,20 @@
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Permissions
+PRODUCT_COPY_FILES := \
+	frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
+	
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.mt6771.rc \
+    fstab.mt6771 
+
+# Inherit vendor
+$(call inherit-product, vendor/realme/lucifer/lucifer-vendor.mk)
