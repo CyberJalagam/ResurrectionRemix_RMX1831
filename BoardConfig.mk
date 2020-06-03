87345222c4c0fd4a8b076b4a85cdce7857b63d25
buddi56 <ynaveenk.0513@gmail.com>
2020-06-04 00:00:05 +0530

    RMX1831: update sepolicy flags and files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,8 +121,8 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 
 -include vendor/realme/lucifer/BoardConfigVendor.mk
 
