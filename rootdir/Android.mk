3247d5b0e738c44b041a9a4303e42136b855e4fb
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2020-06-11 11:58:29 +0530

    RMX1831: rootdir: Change path for init.mt6771.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE       := init.mt6771.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mt6771.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init/hw
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
