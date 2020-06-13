28f3330905f7e3d809236d76ad9141ad2a7f8b62
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2020-06-13 11:41:28 +0530

    RMX1831: Bluetooth: Add more flags

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -3,4 +3,13 @@
 
 #define BTM_DEF_LOCAL_NAME   "Realme U1"
 
+// Disables read remote device feature
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+
+#define BLE_VND_INCLUDED   TRUE
+
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
+
 #endif 
