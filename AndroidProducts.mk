b760832914fc53cf36dbb8a8a45e236d50c48066
JAISHNAVPRASAD-DarklIous <rbinternationalnetwork@gmail.com>
2020-08-12 06:05:49 +0530

    RMX1831: Resrructify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1831.mk
+    $(LOCAL_DIR)/rr_RMX1831.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1831-user \
-    lineage_RMX1831-userdebug \
-    lineage_RMX1831-eng
+    rr_RMX1831-user \
+    rr_RMX1831-userdebug \
+    rr_RMX1831-eng
 
--- rr_RMX1831.mk
+++ rr_RMX1831.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ResrructionRemix stuff
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
@@ -23,5 +23,8 @@ TARGET_VENDOR_PRODUCT_NAME := RMX1831
 PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200605.001 6392402 release-keys"
     
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 
